4d05333cbea6e58e3cf2fb42b19de0d4f5dc0e99
Danila Kulinkovich <danek200326@gmail.com>
2025-06-04 13:48:01 +0300

    commit44441111

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let db;
 
 MongoClient.connect(mongoUrl, { useUnifiedTopology: true })
   .then(client => {
-    db = client.db(); // или client.db('название_базы_данных')
+db = client.db('charity');
     console.log("Connected to MongoDB");
   })
   .catch(err => {
